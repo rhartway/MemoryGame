569b3bc0a6bdc4a471ea2e574fb1946bda733659
Rebecca Hartway <rghartway@wm.edu>
2023-10-05 18:54:05 -0400

    First version of CharacterGamePiece

--- src/edu/wm/cs/cs301/memorygame/CharacterGamePiece.java
+++ src/edu/wm/cs/cs301/memorygame/CharacterGamePiece.java
@@ -2,25 +2,31 @@ package edu.wm.cs.cs301.memorygame;
 
 public class CharacterGamePiece implements GamePiece {
 	private final Character symbol;
+	private boolean visible;
 	
 	public CharacterGamePiece(char s) {
 		this.symbol = s;
+		this.visible = false;
 	}
 
 	public Character getSymbol() {
-
+		return symbol;
 	}
 	
 	public void setVisible(boolean v) {
-
+		this.visible = v;
 	}
 	
 	public boolean isVisible() {
-
+		return this.visible;
 	}
 	
 	public boolean equals(GamePiece other) {
-
+		if (other instanceof CharacterGamePiece) {
+			CharacterGamePiece otherCharacterGamePiece = (CharacterGamePiece) other;
+			return this.symbol.equals(otherCharacterGamePiece.getSymbol());
+		}
+		return false;
 	}
 	
 }
