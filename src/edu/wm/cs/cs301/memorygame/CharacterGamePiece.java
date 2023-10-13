2bb9d8dd76355831049a07c239cd705facc86468
Rebecca Hartway <rghartway@wm.edu>
2023-10-13 00:54:23 -0400

    New CharacterGamePiece

--- src/edu/wm/cs/cs301/memorygame/CharacterGamePiece.java
+++ src/edu/wm/cs/cs301/memorygame/CharacterGamePiece.java
@@ -3,10 +3,13 @@ package edu.wm.cs.cs301.memorygame;
 public class CharacterGamePiece implements GamePiece {
 	private final Character symbol;
 	private boolean visible;
+	private boolean matched; // Added property to track if the card is matched or not
+
 	
 	public CharacterGamePiece(char s) {
 		this.symbol = s;
 		this.visible = false;
+		this.matched = false;
 	}
 
 	public Character getSymbol() {
@@ -20,6 +23,16 @@ public class CharacterGamePiece implements GamePiece {
 	public boolean isVisible() {
 		return this.visible;
 	}
+	
+	@Override
+    public boolean isMatched() {
+        return matched;
+    }
+
+    // Method to set the card as matched
+    public void markAsMatched() {
+        matched = true;
+    }
 	
 	public boolean equals(GamePiece other) {
 		if (other instanceof CharacterGamePiece) {
